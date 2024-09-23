dd213aa1262d578243724b9a8ede9033ca7af73e
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-09-23 09:02:56 +0700

    JS4_Praktikum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,12 @@ class UserController extends Controller
     public function index() {
         // tambah data user dengan Eloqent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data
+        UserModel::create($data);
 
         //coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
