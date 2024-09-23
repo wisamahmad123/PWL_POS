32e70044a2010f05451aae70ccbcf345e7efccf9
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-09-23 09:56:00 +0700

    JS4_Praktikum2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort((404));
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
