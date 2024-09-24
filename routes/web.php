0d8afa5ba65975aa77759a0cf33c472bf65b6efb
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-09-24 13:52:57 +0700

    JS5_Praktikum2

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +29,4 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/', [WelcomeController::class, 'index']);
