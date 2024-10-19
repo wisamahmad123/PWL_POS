3842f596db204d1abd570122f6686e2a4f42c68e
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-10-19 16:32:39 +0700

    uts fix supplier, expor impor

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\SupplierModel;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Validator;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class SupplierController extends Controller
 {
@@ -51,38 +53,6 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';*/
             ->make(true);
     }
 
-     // Menampilkan halaman form tambah supplier 
-     public function create()
-     {
-         $breadcrumb = (object) [
-             'title' => 'Tambah Supplier',
-             'list' => ['Home', 'Supplier', 'Tambah']
-         ];
-         $page = (object) [
-             'title' => 'Tambah supplier baru'
-         ];
-         $activeMenu = 'supplier'; // set menu yang sedang aktif
-         return view('supplier.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
-     }
-
-      // Menyimpan data supplier baru
-    public function store(Request $request)
-    {
-        $request->validate([
-            // Kode harus diisi, berupa string, minimal 3 karakter, dan bernilai unik di tabel m_user kolom username
-            'supplier_kode'  => 'required|string|min:3|max:10|unique:m_supplier,supplier_kode',
-            'supplier_nama'      => 'required|string|max: 100', //nama harus diisi, berupa string, dan maksimal 100 karakter
-            'supplier_alamat' => 'required|string|max: 255', // alamat harus diisi, berupa string, dan maksimal 255 karakter
-        ]);
-
-        SupplierModel::create([
-            'supplier_kode'  => $request->supplier_kode,
-            'supplier_nama'      => $request->supplier_nama,
-            'supplier_alamat'   => $request->supplier_alamat,
-        ]);
-        return redirect('/supplier')->with('success', 'Data supplier berhasil disimpan');
-    }
-
     // Menampilkan detail user
     public function show(string $id)
     {
@@ -96,57 +66,6 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';*/
         ];
         $activeMenu = 'supplier'; // set menu yang sedang aktif
         return view('supplier.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
-    }
-
-    // Menampilkan halaman fore edit user 
-    public function edit(string $id)
-    {
-        $supplier = SupplierModel::find($id);
-        $breadcrumb = (object) [
-            'title' => 'Edit Supplier',
-            'list' => ['Home', 'Supplier', 'Edit']
-        ];
-
-        $page = (object) [
-            "title" => 'Edit supplier'
-        ];
-
-        $activeMenu = 'supplier'; // set menu yang sedang aktif
-        return view('supplier.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'supplier' => $supplier, 'supplier' => $supplier, 'activeMenu' => $activeMenu]);
-    }
-
-    // Menyimpan perubahan data user
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            // supplier_kode harus diisi, berupa string, minimal 3 karakter,
-            // dan bernilai unik di tabel_supplier kolom supplier_kode kecuali untuk supplier dengan id yang sedang diedit
-            'supplier_kode'  => 'required|string|min:3|unique:m_supplier,supplier_kode,' . $id . ',supplier_id',
-            'supplier_nama'      => 'required|string|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
-            'supplier_alamat' => 'required|string|max: 255', // alamat harus diisi, berupa string, dan maksimal 255 karakter
-        ]);
-        supplierModel::find($id)->update([
-            'supplier_kode'  => $request->supplier_kode,
-            'supplier_nama'      => $request->supplier_nama,
-            'supplier_alamat'   => $request->supplier_alamat,
-        ]);
-        return redirect('/supplier')->with("success", "Data supplier berhasil diubah");
-    }
-
-    // Menghapus data user 
-    public function destroy(string $id)
-    {
-        $check = SupplierModel::find($id);
-        if (!$check) {      // untuk mengecek apakah data supplier dengan id yang dimaksud ada atau tidak
-            return redirect('/supplier')->with('error', 'Data supplier tidak ditemukan');
-        }
-        try {
-            SupplierModel::destroy($id); // Hapus data supplier
-            return redirect('/supplier')->with('success', 'Data supplier berhasil dihapus');
-        } catch (\Illuminate\Database\QueryException $e) {
-            // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
-            return redirect('/supplier')->with('error', 'Data supplier gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
-        }
     }
 
     public function create_ajax()
@@ -248,5 +167,112 @@ confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';*/
             }
         }
         return redirect('/');
+    }
+
+    public function import()
+    {
+        return view('supplier.import');
+    }
+    public function import_ajax(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                // validasi file harus xls atau xlsx, max 1MB
+                'file_supplier' => ['required', 'mimes:xlsx', 'max:1024']
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+            $file = $request->file('file_supplier'); // ambil file dari request
+            $reader = IOFactory::createReader('Xlsx'); // load reader file excel
+            $reader->setReadDataOnly(true); // hanya membaca data
+            $spreadsheet = $reader->load($file->getRealPath()); // load file excel
+            $sheet = $spreadsheet->getActiveSheet(); // ambil sheet yang aktif
+            $data = $sheet->toArray(null, false, true, true); // ambil data excel
+            $insert = [];
+            if (count($data) > 1) { // jika data lebih dari 1 baris
+                foreach ($data as $baris => $value) {
+                    if ($baris > 1) { // baris ke 1 adalah header, maka lewati
+                        $insert[] = [
+                            'supplier_kode' => $value['A'],
+                            'supplier_nama' => $value['B'],
+                            'supplier_alamat' => $value['C'],
+                            'created_at' => now(),
+                        ];
+                    }
+                }
+                if (count($insert) > 0) {
+                    // insert data ke database, jika data sudah ada, maka diabaikan
+                    SupplierModel::insertOrIgnore($insert);
+                }
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diimport'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Tidak ada data yang diimport'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+    public function export_excel()
+    {
+        // ambil data supplier yang akan di export
+        $supplier = SupplierModel::select('supplier_kode', 'supplier_nama', 'supplier_alamat')
+            ->get();
+        // load library excel
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet(); // ambil sheet yang aktif
+        $sheet->setCellValue('A1', 'No');
+        $sheet->setCellValue('B1', 'Kode supplier');
+        $sheet->setCellValue('C1', 'Nama supplier');
+        $sheet->setCellValue('D1', 'Alamat supplier');
+        $sheet->getStyle('A1:D1')->getFont()->setBold(true); // bold header
+
+        $no = 1; // nomor data dimulai dari 1
+        $baris = 2; // baris data dimulai dari baris ke 2
+        foreach ($supplier as $key => $value) {
+            $sheet->setCellValue('A' . $baris, $no);
+            $sheet->setCellValue('B' . $baris, $value->supplier_kode);
+            $sheet->setCellValue('C' . $baris, $value->supplier_nama);
+            $sheet->setCellValue('D' . $baris, $value->supplier_alamat);
+            $baris++;
+            $no++;
+        }
+        foreach (range('A', 'D') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true); // set auto size untuk kolom
+        }
+        $sheet->setTitle('Data Supplier'); // set title sheet
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $filename = 'Data supplier ' . date('Y-m-d H:i:s') . '.xlsx';
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Last-Modified:' . gmdate('D, d M Y H:i:s') . ' GMT');
+        header('Cache-Control: cache, must-revalidate');
+        header('Pragma: public');
+        $writer->save('php://output');
+        exit;
+    } // end function export_excel
+
+    public function export_pdf()
+    {
+        $supplier = SupplierModel::select('supplier_kode', 'supplier_nama', 'supplier_alamat')->orderBy('supplier_kode')->get();
+        // use Barryvdh\DomPDF\Facade\Pdf;
+        $pdf = Pdf::loadView('supplier.export_pdf', ['supplier' => $supplier]);
+        $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+        $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url $pdf->render();
+        return $pdf->stream('Data Supplier' . date('Y-m-d H:i:s') . '.pdf');
     }
 }
--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -2,10 +2,12 @@
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
+            <h3 class="card-title">Daftar Supplier</h3>
             <div class="card-tools">
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-info">Import Supplier</button>
+                <a href="{{ url('/supplier/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export Supplier</a>
+                <a href="{{ url('/supplier/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export Supplier</a>
+                <button onclick="modalAction('{{ url('/supplier/create_ajax') }}')" class="btn btn-success">Tambah Data(Ajax)</button>
             </div>
         </div>
         <div class="card-body">
