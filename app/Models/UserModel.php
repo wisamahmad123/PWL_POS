10159ad4bed3ac861753b944534ecac4f9eeaf49
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-10-28 10:18:24 +0700

    JS10_Praktikum2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;  //implementasi class authenticatable
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
